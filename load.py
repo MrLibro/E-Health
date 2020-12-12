1b652d33ee9820d4ffd059b2dd4ce2c1daa6f5f8
Alireza Ketabdari <56600625+MrLibro@users.noreply.github.com>
2020-12-12 03:32:57 +0100

    Add files via upload

---
+++ load.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+def load_db(name: str, dbORdic):
+    if dbORdic == 'database':
+        pathfile = 'databases/' + name + '.csv'
+    elif dbORdic == "dictionary":
+        pathfile = 'dictionaries/' + name + '.csv'
+    file_db = pd.read_csv(pathfile, low_memory=False)
+    return (file_db)
